f32a4ed7c047b0855eb4102267615305217837f0
ganeshakula98 <48818961+ganeshakula98@users.noreply.github.com>
2024-02-15 14:43:45 -0600

    Update existing file

--- main3.tf
+++ main3.tf
@@ -1,17 +1,1 @@
-
-
-provider "aws" {
-  region = "us-east-1"  # Change to your desired AWS region
-}
-
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = "your-unique-bucket-name"  # Change to a unique bucket name
-
-  acl    = "private"
-
-  tags = {
-    Name        = "ExampleBucket"
-    Environment = "Development"
-  }
-}
-
+main.txt file 1
