23ac551419507ae6b9e3cab3dad2f448e43a4da0
ganeshakula98 <48818961+ganeshakula98@users.noreply.github.com>
2024-02-20 13:26:36 -0600

    creat/modify the file

--- path/to/file.tf
+++ path/to/file.tf
@@ -7,3 +7,13 @@ resource "azurerm_resource_group" "example" {
     name     = {{var.resource_group_name}}
     location = {{var.location}}
   }
+
+resource "azurerm_resource_group" "example" {
+    name     = {{var.resource_group_name}}
+    location = {{var.location}}
+  }
+
+resource "azurerm_resource_group" "example" {
+    name     = {{var.resource_group_name}}
+    location = {{var.location}}
+  }
